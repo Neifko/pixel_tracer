fd3e1f11471724e6157f47a287336329aabea6a6
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2025-05-07 12:16:36 +0200

    add area

--- src/main/java/fr/neifko/area/Area.java
+++ src/main/java/fr/neifko/area/Area.java
@@ -1,63 +1,174 @@
 package fr.neifko.area;
 
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * 
+ * Classe représentant une zone (Area).
  */
 public class Area {
 
     /**
-     * Default constructor
-     */
-    public Area() {
-    }
-
-    /**
-     * 
+     * Identifiant unique de la zone.
      */
     protected int id;
 
     /**
-     * 
+     * Nom de la zone.
      */
-    protected string name;
-
+    protected String name;
 
     /**
-     * 
+     * Largeur de la zone.
+     */
+    protected int width;
+
+    /**
+     * Hauteur de la zone.
+     */
+    protected int height;
+
+    /**
+     * Caractère représentant une case vide.
+     */
+    protected char emptyChar = '.';
+
+    /**
+     * Caractère représentant une case pleine.
+     */
+    protected char fullChar = '@';
+
+    /**
+     * Matrice représentant la zone.
+     */
+    protected char[][] area;
+
+    /**
+     * Liste des couches associées à la zone.
+     */
+    protected List<Layer> lstLayers;
+
+    /**
+     * Constructeur par défaut.
      */
     public Area() {
-        // TODO implement here
+        this.lstLayers = new ArrayList<>();
     }
 
     /**
-     * 
+     * Constructeur avec paramètres.
+     *
+     * @param id       Identifiant de la zone.
+     * @param name     Nom de la zone.
+     * @param width    Largeur de la zone.
+     * @param height   Hauteur de la zone.
      */
-    public void getId() {
-        // TODO implement here
+    public Area(int id, String name, int width, int height) {
+        this.id = id;
+        this.name = name;
+        this.width = width;
+        this.height = height;
+        this.emptyChar = '.';
+        this.fullChar = '@';
+        this.lstLayers = new ArrayList<>();
+        this.area = new char[height][width];
+
+        // Initialisation de la matrice avec le caractère vide
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                this.area[i][j] = emptyChar;
+            }
+        }
+    }
+
+    // Getters et setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public char getEmptyChar() {
+        return emptyChar;
+    }
+
+    public void setEmptyChar(char emptyChar) {
+        this.emptyChar = emptyChar;
+    }
+
+    public char getFullChar() {
+        return fullChar;
+    }
+
+    public void setFullChar(char fullChar) {
+        this.fullChar = fullChar;
+    }
+
+    public char[][] getArea() {
+        return area;
+    }
+
+    public void setArea(char[][] area) {
+        this.area = area;
+    }
+
+    public List<Layer> getLstLayers() {
+        return lstLayers;
+    }
+
+    public void setLstLayers(List<Layer> lstLayers) {
+        this.lstLayers = lstLayers;
     }
 
     /**
-     * 
+     * Ajoute une couche à la liste des couches.
+     *
+     * @param layer La couche à ajouter.
      */
-    public void setId() {
-        // TODO implement here
+    public void addLayer(Layer layer) {
+        this.lstLayers.add(layer);
     }
 
     /**
-     * 
+     * Supprime une couche de la liste des couches.
+     *
+     * @param layer La couche à supprimer.
      */
-    public void getName() {
-        // TODO implement here
+    public void removeLayer(Layer layer) {
+        this.lstLayers.remove(layer);
     }
 
     /**
-     * 
+     * Supprime toutes les couches associées à la zone.
      */
-    public void setName() {
-        // TODO implement here
+    public void deleteAllLayers() {
+        this.lstLayers.clear();
     }
-
 }
--- src/main/java/fr/neifko/area/AreaList.java
+++ src/main/java/fr/neifko/area/AreaList.java
@@ -1,44 +1,65 @@
 package fr.neifko.area;
 
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * 
+ * Classe représentant une liste de zones (AreaList).
  */
 public class AreaList {
 
     /**
-     * Default constructor
+     * Liste des zones.
+     */
+    protected List<Area> areas;
+
+    /**
+     * Constructeur par défaut.
      */
     public AreaList() {
+        this.areas = new ArrayList<>();
     }
 
     /**
-     * 
+     * Ajoute une zone à la liste.
+     *
+     * @param area La zone à ajouter.
      */
-    protected list areas;
-
-
-    /**
-     * 
-     */
-    public void addArea() {
-        // TODO implement here
+    public void addArea(Area area) {
+        this.areas.add(area);
     }
 
     /**
-     * 
+     * Supprime une zone de la liste.
+     *
+     * @param area La zone à supprimer.
      */
-    public void removeArea() {
-        // TODO implement here
+    public void removeArea(Area area) {
+        this.areas.remove(area);
     }
 
     /**
-     * 
+     * Supprime toutes les zones de la liste.
      */
     public void deleteAllAreas() {
-        // TODO implement here
+        this.areas.clear();
     }
 
+    /**
+     * Retourne la liste des zones.
+     *
+     * @return La liste des zones.
+     */
+    public List<Area> getAreas() {
+        return areas;
+    }
+
+    /**
+     * Définit la liste des zones.
+     *
+     * @param areas La nouvelle liste des zones.
+     */
+    public void setAreas(List<Area> areas) {
+        this.areas = areas;
+    }
 }

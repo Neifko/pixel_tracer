4e22306a04db84cd85bdfb2e1f1d3351c9a9c11b
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2025-05-07 12:11:45 +0200

    add area

---
+++ src/main/java/fr/neifko/area/Area.java
@@ -0,0 +1,63 @@
+package fr.neifko.area;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Area {
+
+    /**
+     * Default constructor
+     */
+    public Area() {
+    }
+
+    /**
+     * 
+     */
+    protected int id;
+
+    /**
+     * 
+     */
+    protected string name;
+
+
+    /**
+     * 
+     */
+    public Area() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void getId() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void setId() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void getName() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void setName() {
+        // TODO implement here
+    }
+
+}
---
+++ src/main/java/fr/neifko/area/AreaList.java
@@ -0,0 +1,44 @@
+package fr.neifko.area;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class AreaList {
+
+    /**
+     * Default constructor
+     */
+    public AreaList() {
+    }
+
+    /**
+     * 
+     */
+    protected list areas;
+
+
+    /**
+     * 
+     */
+    public void addArea() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void removeArea() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void deleteAllAreas() {
+        // TODO implement here
+    }
+
+}

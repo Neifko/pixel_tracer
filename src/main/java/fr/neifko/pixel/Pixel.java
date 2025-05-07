b4b8e39ebcfaf1dcb0f12f69b946c6b8fbaf75fa
Ostap <ostap.tymchyshyn.92@gmail.com>
2025-05-07 11:23:54 +0200

    ajout de Pixel.java

---
+++ src/main/java/fr/neifko/pixel/Pixel.java
@@ -0,0 +1,105 @@
+package fr.neifko.pixel;
+
+
+
+/**
+ * Class Pixel
+ */
+public class Pixel {
+
+    //
+    // Fields
+    //
+  
+    
+    //
+    // Constructors
+    //
+    public Pixel () { };
+    
+    //
+    // Methods
+    //
+  
+  
+    //
+    // Accessor methods
+    //
+  
+    //
+    // Other methods
+    //
+  
+    /**
+     * @param        pixel
+     */
+    private void delete_pixel(Pixel pixel)
+    {
+    }
+  
+  
+    /**
+     * @param        shape
+     * @param        lst
+     */
+    private void pixel_point(Shape shape, list lst)
+    {
+    }
+  
+  
+    /**
+     * @param        shape
+     * @param        lst
+     */
+    private void pixel_line(Shape shape, list lst)
+    {
+    }
+  
+  
+    /**
+     * @param        shape
+     * @param        lst
+     */
+    private void pixel_cercle(Shape shape, list lst)
+    {
+    }
+  
+  
+    /**
+     * @param        shape
+     * @param        lst
+     */
+    private void pixel_rectangle(Shape shape, list lst)
+    {
+    }
+  
+  
+    /**
+     * @param        shape
+     * @param        lst
+     */
+    private void pixel_square(Shape shape, list lst)
+    {
+    }
+  
+  
+    /**
+     * @param        shape
+     * @param        lst
+     */
+    private void pixel_polygon(Shape shape, list lst)
+    {
+    }
+  
+  
+    /**
+     * @param        shape
+     * @param        lst
+     */
+    private void pixel_curve(Shape shape, list lst)
+    {
+    }
+  
+  
+  }
+  

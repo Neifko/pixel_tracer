859a660c7fbef33fe320047680fb997b8b50c753
xavierli901 <xavierli901@gmail.com>
2025-05-07 12:06:45 +0200

    package render

---
+++ src/main/java/fr/neifko/render/Render.java
@@ -0,0 +1,71 @@
+package fr.neifko.render;
+
+/**
+ * Class Render
+ */
+public class Render{
+
+    //
+    // Fields
+    //
+
+
+    //
+    // Constructors
+    //
+    public Render () { };
+
+    //
+    // Methods
+    //
+
+
+    //
+    // Accessor methods
+    //
+
+    //
+    // Other methods
+    //
+
+    /**
+     * @param area
+     */
+    private void render_area(Area area)
+    {
+    }
+
+
+    /**
+     * @param area
+     */
+    private void draw_area(Area area)
+    {
+    }
+
+
+    /**
+     */
+    private void clear_screen()
+    {
+    }
+
+
+    /**
+     * @param area
+     * @param layer
+     */
+    private void draw_layer_shapes(Area area, Layer layer)
+    {
+    }
+
+
+    /**
+     * @param area
+     */
+    private void draw_all_layers(Area area)
+    {
+    }
+
+
+}

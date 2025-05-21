e9c832cfc180a07c64454d79ec1fe64b77294570
ostacheGIT <ostap.tymchyshyn.92@gmail.com>
2025-05-21 09:42:09 +0200

    corrections Pixel.java

--- src/main/java/fr/neifko/pixel/Pixel.java
+++ src/main/java/fr/neifko/pixel/Pixel.java
@@ -1,105 +1,69 @@
 package fr.neifko.pixel;
 
+import fr.neifko.shape.Shape;
 
+import java.util.List;
 
 /**
  * Class Pixel
  */
 public class Pixel {
-
-    //
-    // Fields
-    //
-  
-    
-    //
-    // Constructors
-    //
     public Pixel () { };
-    
-    //
-    // Methods
-    //
-  
-  
-    //
-    // Accessor methods
-    //
-  
-    //
-    // Other methods
-    //
-  
+
     /**
      * @param        pixel
      */
     private void delete_pixel(Pixel pixel)
     {
     }
-  
-  
     /**
      * @param        shape
      * @param        lst
      */
-    private void pixel_point(Shape shape, list lst)
+    private void pixel_point(Shape shape, List lst)
     {
     }
-  
-  
     /**
      * @param        shape
      * @param        lst
      */
-    private void pixel_line(Shape shape, list lst)
+    private void pixel_line(Shape shape, List lst)
     {
     }
-  
-  
     /**
      * @param        shape
      * @param        lst
      */
-    private void pixel_cercle(Shape shape, list lst)
+    private void pixel_cercle(Shape shape, List lst)
     {
     }
-  
-  
     /**
      * @param        shape
      * @param        lst
      */
-    private void pixel_rectangle(Shape shape, list lst)
+    private void pixel_rectangle(Shape shape, List lst)
     {
     }
-  
-  
     /**
      * @param        shape
      * @param        lst
      */
-    private void pixel_square(Shape shape, list lst)
+    private void pixel_square(Shape shape, List lst)
     {
     }
-  
-  
     /**
      * @param        shape
      * @param        lst
      */
-    private void pixel_polygon(Shape shape, list lst)
+    private void pixel_polygon(Shape shape, List lst)
     {
     }
-  
-  
     /**
      * @param        shape
      * @param        lst
      */
-    private void pixel_curve(Shape shape, list lst)
+    private void pixel_curve(Shape shape, List lst)
     {
     }
-  
-  
   }
   

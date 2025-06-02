a41934b2c799dd9a5cfce71330a0626a197421e0
Alexis <alexie78180@gmail.com>
2025-06-02 11:50:14 +0200

    command done

--- src/main/java/fr/neifko/command/Command.java
+++ src/main/java/fr/neifko/command/Command.java
@@ -4,7 +4,7 @@ import fr.neifko.App;
 import fr.neifko.Main;
 import fr.neifko.area.Area;
 import fr.neifko.layer.Layer;
-import fr.neifko.shape.Shape;
+import fr.neifko.shape.*;
 
 public class Command {
 
@@ -47,13 +47,14 @@ public class Command {
         // first token is the name of the command
         name = tokens[0];
 
+        str_params[0] = name;
+        str_size = 1;
+
         for (int i = 1; i < tokens.length; i++) {
             if (tokens[i].matches("-?\\d+")) {
-                // if the token is an integer
                 int_params[int_size] = Integer.parseInt(tokens[i]);
                 int_size++;
             } else {
-                // if the token is a string
                 str_params[str_size] = tokens[i];
                 str_size++;
             }
@@ -61,52 +62,122 @@ public class Command {
 
     }
 
-    public void read_exec_command(App app) {
-        if (this.name.equals("exit")) {
+    public int read_exec_command(App app) {
+        read_from_stdin();
+        int errorNum = 1;
+
+        if (str_size == 0) {
+            errorNum = 2;
+            System.out.println("Erreur : pas de commande.");
+            return errorNum;
+        }
+
+        String cmdName = str_params[0];
+
+        if (cmdName.equals("exit")) {
             System.out.println("Exiting...");
-            System.exit(0);
-        }
-        if (this.name.equals("help")) {
+            errorNum = 4;
+        } else if (cmdName.equals("clear")) {
+            errorNum = 5;
+        } else if (cmdName.equals("plot")) {
+            errorNum = 6;
+        } else if (cmdName.equals("help")) {
+            print_help();
             System.out.println("Available commands: exit, help");
-            return;
-        }
-        if (this.name.equals("line")) {
-            System.out.println("Draw line...");
-            return;
-        }
-        if (this.name.equals("circle")) {
-            System.out.println("Draw circle...");
-            return;
-        }
-        if (this.name.equals("rectangle")) {
-            System.out.println("Draw rectangle...");
-            return;
-        }
-
-        if (this.name.equals("new")) {
-            if (this.str_params[0].equals("area")) {
-                System.out.println("New area...");
-                Area newArea = new Area(0,"New Area", 10, 10);
-                area = newArea;
-            } else if (this.str_params[0].equals("layer")) {
-                System.out.println("New layer...");
-                Layer newLayer = new Layer(0,"New Layer");
-                if (area != null) {
-                    area.addLayer(newLayer);
-                }
-                layer = newLayer;
-            } else if (this.str_params[0].equals("shape")) {
-                System.out.println("New shape...");
-                Shape newShape = new Shape();
-                if (layer != null) {
-                    layer.addShape(newShape);
-                }
-                shape = newShape;
+            errorNum = 7;
+        } else if (cmdName.equals("point")) {
+            if (int_size < 2) { errorNum = 3; }
+            else {
+                Point pt = new Point(int_params[0], int_params[1]);
+                System.out.println("Point created: " + pt);
+                errorNum = 0;
             }
-            return;
+        } else if (cmdName.equals("line")) {
+            if (int_size < 4) { errorNum = 3; }
+            else {
+                Line ln = new Line(new Point(int_params[0], int_params[1]), new Point(int_params[2], int_params[3]));
+                errorNum = 0;
+            }
+        } else if (cmdName.equals("circle")) {
+            if (int_size < 3) { errorNum = 3; }
+            else {
+                Circle sh = new Circle(new Point(int_params[0], int_params[1]), int_params[2]);
+                errorNum = 0;
+            }
+        } else if (cmdName.equals("square")) {
+            if (int_size < 3) { errorNum = 3; }
+            else {
+                Squar sh = new Squar(new Point(int_params[0], int_params[1]), int_params[2]);
+                errorNum = 0;
+            }
+        } else if (cmdName.equals("rectangle")) {
+            if (int_size < 4) { errorNum = 3; }
+            else {
+                Rectangle sh = new Rectangle(new Point(int_params[0], int_params[1]), int_params[2], int_params[3]);
+                errorNum = 0;
+            }
+        } else if (cmdName.equals("polygon")) {
+            if (int_size < 6) { errorNum = 3; }
+            else {
+                System.out.println("TODO");
+                errorNum = 0;
+            }
+        } else if (cmdName.equals("curve")) {
+            if (int_size < 8) { errorNum = 3; }
+            else {
+                Curve sh = new Curve(
+                        new Point(int_params[0], int_params[1]),
+                        new Point(int_params[2], int_params[3]),
+                        new Point(int_params[4], int_params[5]),
+                        new Point(int_params[6], int_params[7])
+                );
+                errorNum = 0;
+            }
+        } else if (cmdName.equals("list")) {
+            if (str_size < 2) { errorNum = 3; }
+            else if (str_params[1].equals("areas")) {
+                System.out.println("TODO: List areas");
+                errorNum = 8;
+            } else if (str_params[1].equals("layers")) {
+                System.out.println("TODO: List layers");
+                errorNum = 8;
+            } else if (str_params[1].equals("shapes")) {
+                System.out.println("TODO: List shapes");
+                errorNum = 8;
+            }
+        } else if (cmdName.equals("new")) {
+            if (str_size < 2) { errorNum = 3; }
+            else if (str_params[1].equals("area")) {
+                Area area = new Area(80, "area_name", 80, 40);
+                Area currentArea = app.currentArea;
+                app.currentArea = area;
+                Layer layer = new Layer(0, "Layer 1");
+                area.addLayer(layer);
+                app.currentLayer = layer;
+                app.currentShape = null;
+                errorNum = 8;
+            } else if (str_params[1].equals("layer")) {
+                Layer layer = new Layer(0, "layer_name");
+                app.currentArea.addLayer(layer);
+                app.currentLayer = layer;
+                app.currentShape = null;
+                errorNum = 8;
+            }
+        } else if (cmdName.equals("select")) {
+            // À compléter selon la logique C, pour area/layer/shape
+            errorNum = 8;
+        } else if (cmdName.equals("delete")) {
+            // À compléter selon la logique C, pour shape
+            errorNum = 8;
+        } else if (cmdName.equals("set")) {
+            // À compléter selon la logique C, pour char/layer
+            errorNum = 0;
+        } else {
+            System.out.println("Commande inconnue : " + cmdName);
         }
-        end:
-        System.out.println("Unknown command: " + this.name);
 
+        // Affichage du message d'erreur ou de succès
+        System.out.println("Code retour : " + errorNum);
+        return errorNum;
     }
 }
